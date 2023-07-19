e66ad41ef775599870d9b16b7134e2051b667826
bsu7959 <bsu7959@gmail.com>
2023-07-19 20:49:54 +0900

    페이지넘기며 타이틀 가져오기

--- src/server/fetching.js
+++ src/server/fetching.js
@@ -1,14 +1,45 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
+const puppeteer = require('puppeteer');
 
-let $title = [];
-axios.get(`https://www.whufc.com/news`)
-    .then(data => {
-        const $ = cheerio.load(data.data);
-        $('div.o-news__item h3.m-teaser__content-title').each((index, item) => {
-            $title.push(item.children[0].data) 
+
+(async () => {
+
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://www.whufc.com/news?category=All&page=0');
+
+    while (true) {
+
+        const articles = await page.evaluate(() => {
+            const articles = [];
+            document.querySelectorAll('.m-teaser__link').forEach((a) => {
+                articles.push({
+                    title: a.textContent.trim(),
+                    link: a.href,
+                });
+            });
+            console.log(document.querySelector('.m-pagination__icon--next a'))
+            return articles;
         });
-        console.log($title);
-    });
+        console.log(articles);
 
- 
+
+        //if(!articles[1]) break;
+        //page.click('.m-pagination__icon--next a');
+        page.waitForNavigation();
+        await page.click('.m-pagination__icon--next a')
+        await page.waitForSelector('.m-pagination__icon--next a');
+
+    }
+
+
+
+    // // const pagination = document.querySelectorAll('.m-pagination__icon--next');
+    // // console.log('pagination')
+    // // //if (pagination == undefined) break;
+    // // page.click(pagination);
+    // // console.log(articles);
+    // // console.log('---------------------------------');
+    // browser.click(document.querySelectorAll('.m-pagination__icon--next')[0])
+
+    await browser.close();
+})();
